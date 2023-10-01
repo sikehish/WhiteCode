0ea2349a51c366bcdd7c6a7920760d51f4e8749a
Syed Hisham Akmal <hisham0502@gmail.com>
2023-10-01 21:06:35 +0530

    set https:true

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,10 +8,12 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    https: true,
     proxy: {
       "/api": {
         target: "http://127.0.0.1:3000",
         changeOrigin: true,
+
         secure: false,
       },
     },
